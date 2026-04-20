1ab5d1b7a21e147e4b9d9aace3a92bdc61529e32
Daniel Hosomi <danirtdhosomi@gmail.com>
2026-04-20 14:29:17 -0300

    fix

--- project2/src/geo_sample_merger.py
+++ project2/src/geo_sample_merger.py
@@ -3,29 +3,24 @@ import glob
 import os
 
 arquivos = glob.glob("../data/GSE296007_RAW/*.txt.gz")
-dados_agregados = {}
 
+dados_amostras = {}
 for arquivo in arquivos:
     nome_base = os.path.basename(arquivo)
+    
     partes = nome_base.split("_")
-    
-    if len(partes) >= 2:
-        grupo_tratamento = partes[1] 
+    if len(partes) >= 3:
+        nome_coluna = f"{partes[1]}_{partes[2]}" 
     else:
-        grupo_tratamento = "Desconhecido"
-    
+        nome_coluna = partes[0]
+        
     df = pd.read_csv(arquivo, sep="\t", header=None, names=["gene_id", "contagem"], compression="gzip")
     df = df[~df["gene_id"].astype(str).str.startswith("__")]
     
-    serie_contagem = df.set_index("gene_id")["contagem"]
-    
-    if grupo_tratamento in dados_agregados:
-        dados_agregados[grupo_tratamento] = dados_agregados[grupo_tratamento].add(serie_contagem, fill_value=0)
-    else:        dados_agregados[grupo_tratamento] = serie_contagem
+    dados_amostras[nome_coluna] = df.set_index("gene_id")["contagem"]
 
-tabela_final = pd.DataFrame(dados_agregados)
-tabela_final = tabela_final.fillna(0).astype(int)
+tabela_final = pd.DataFrame(dados_amostras)
 tabela_final.reset_index(inplace=True)
 
-nome_saida = "../data/tabela_expressao_genica_microplastico.csv"
+nome_saida = "../data/tabela_expressao_microplasticos.csv"
 tabela_final.to_csv(nome_saida, index=False)
