02f4c906649d591e74ebae044435d18fcab18481
Daniel Hosomi <danirtdhosomi@gmail.com>
2026-04-21 16:01:44 -0300

    chore(): add pipeline to generate filtered genes per concentration

---
+++ project2/src/io_transforms.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import pandas as pd
+
+
+def prepare_GEO_dataset(input_directory: str, output_path: str) -> pd.DataFrame:
+    search_pattern = os.path.join(input_directory, "*.txt.gz")
+    files = glob.glob(search_pattern)
+
+    sample_data = {}
+
+    for file in files:
+        file_name = os.path.basename(file)
+        column_name = file_name.replace(".txt.gz", "").replace("_count", "")
+
+        parts = column_name.split("_")
+        if len(parts) >= 2:
+            column_name = "_".join(parts[1:])
+        else:
+            column_name = column_name
+
+        df = pd.read_csv(
+            file,
+            sep="\t",
+            header=None,
+            names=["gene_id", "count"],
+            compression="gzip",
+        )
+        df = df[~df["gene_id"].astype(str).str.startswith("__")]
+
+        sample_data[column_name] = df.set_index("gene_id")["count"]
+
+    final_table = pd.DataFrame(sample_data).fillna(0).astype(int)
+    final_table.reset_index(inplace=True)
+
+    return final_table
+
+
+def load_dataframe(file_path: str) -> pd.DataFrame:
+    return pd.read_csv(file_path)
+
+
+def save_dataframe(df: pd.DataFrame, file_path: str):
+    df.to_csv(file_path, index=False)
---
+++ project2/src/metrics.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+
+
+def calculate_log2fc(
+    df: pd.DataFrame, treatment_group: str, control_group: str = "CTR"
+) -> pd.DataFrame:
+    treatment_columns = [
+        col for col in df.columns if col.startswith(f"{treatment_group}_")
+    ]
+    control_columns = [
+        col for col in df.columns if col.startswith(f"{control_group}_")
+    ]
+
+    treatment_mean = df[treatment_columns].mean(axis=1) + 1
+    control_mean = df[control_columns].mean(axis=1) + 1
+    log2fc_values = np.log2(treatment_mean / control_mean)
+
+    fc_result = pd.DataFrame(
+        {"gene_id": df["gene_id"], f"Log2FC_{treatment_group}": log2fc_values}
+    )
+
+    return fc_result
+
+def filter_significant_genes(
+    df: pd.DataFrame, target_column: str, threshold: float = 1.0
+) -> pd.DataFrame:
+    filtered_genes = df[
+        (df[target_column] >= threshold) | (df[target_column] <= -threshold)
+    ].copy()
+
+    original_count = len(df)
+    final_count = len(filtered_genes)
+    print(f"Genes discarded: {original_count - final_count}")
+
+    return filtered_genes
